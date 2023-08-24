57d1f91e5c66cbba24898970127bebec6c2334a5
Pomazzanus <pomazan.nikita.ik02@gmail.com>
2023-08-24 18:54:41 +0300

    Delete unnecessary code, add repo and update findById method in controller

--- src/main/java/com/example/PiB/MainController.java
+++ src/main/java/com/example/PiB/MainController.java
@@ -1,23 +1,36 @@
 package com.example.PiB;
 
+//import com.example.PiB.Repository.PetRepository;
+import com.example.PiB.Repository.PetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/animal")
+@RequestMapping("/pet")
 public class MainController {
-    @GetMapping("/{petId}")
-    public ResponseEntity<Pet> findById(){
-        Pet doggo = new Pet(1L, "Antoha");
-        return ResponseEntity.ok(doggo);
+    private PetRepository petRepo;
+
+    public MainController(PetRepository petRepository){
+        this.petRepo = petRepository;
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Pet> findById(@PathVariable Long id){
+        Optional<Pet> petOptional = petRepo.findById(id);
+        if (petOptional.isPresent()) {
+            return ResponseEntity.ok(petOptional.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
-    @GetMapping("/")
+    /*@GetMapping("/")
     public ResponseEntity<List<Pet>> getAll(){
         Pet doggo = new Pet(1L, "Antoha");
         Pet catto = new Pet(2L, "Artemka");
@@ -30,4 +43,13 @@ public class MainController {
         pets.add(cattonio);
         return ResponseEntity.ok(pets);
     }
+    @PostMapping
+    private ResponseEntity<Void> createCashCard(@RequestBody Pet newPetRequest, UriComponentsBuilder ucb) {
+        Pet savedPet = PetRepository.save(newPetRequest);
+        URI locationOfNewPet = ucb
+                .path("Pet/{petId}")
+                .buildAndExpand(savedPet.getId())
+                .toUri();
+        return ResponseEntity.created(locationOfNewPet).build();
+    } */
 }
