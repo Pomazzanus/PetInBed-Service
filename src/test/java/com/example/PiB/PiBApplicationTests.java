f1e2ba948cbd461a71623b49c23ae3f7fb71e752
Pomazzanus <pomazan.nikita.ik02@gmail.com>
2023-08-24 19:47:51 +0300

    Add new Pet creation test

--- src/test/java/com/example/PiB/PiBApplicationTests.java
+++ src/test/java/com/example/PiB/PiBApplicationTests.java
@@ -7,6 +7,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.net.URI;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -30,5 +33,21 @@ class PiBApplicationTests {
 		ResponseEntity<String> response = restTemplate.getForEntity("/pet/1000", String.class);
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
 		assertThat(response.getBody()).isBlank();
+	}
+
+	@Test
+	void shouldCreateANewPet() {
+		Pet newPet = new Pet(null, "Dominic");
+		ResponseEntity<Void> createResponse = restTemplate.postForEntity("/pet", newPet, Void.class);
+		assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+		URI locationOfNewCashCard = createResponse.getHeaders().getLocation();
+		ResponseEntity<String> getResponse = restTemplate.getForEntity(locationOfNewCashCard, String.class);
+		assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		DocumentContext documentContext = JsonPath.parse(getResponse.getBody());
+		Number id = documentContext.read("$.id");
+		String petName = documentContext.read("$.petName");
+		assertThat(id).isNotNull();
+		assertThat(petName).isEqualTo("Dominic");
 	}
 }
